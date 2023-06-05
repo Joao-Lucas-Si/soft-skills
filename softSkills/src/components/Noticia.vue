5e7c327d36bfbd9e8348a048e09c7e030f5e2af0
joao <joao6lucas2168@gmail.com>
2023-06-05 20:07:48 -0300

    iniciação do projeto

---
+++ softSkills/src/components/Noticia.vue
@@ -0,0 +1,79 @@
+<template>
+<div class="noticia">
+    <a class="fundo" :href="url">
+        <img :src="img" alt="">
+    </a>
+    <div class="canto">
+        <a :href="fonteUrl">
+            <img :src="fonteLogo" alt="">
+        </a>
+        <h2>{{ titulo }}</h2>
+    </div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default {
+    props: {
+        url: String,
+        img: String,
+        posicao: Number,
+        fonteUrl: String,
+        fonteLogo: String,
+        noticiaAtual: Number,
+        titulo: String,
+        cor: String,
+    }, 
+    computed: {
+        cssVars() {
+            return {
+                '--noticia-cor': this.cor
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.noticia {
+    width: 100%;
+    height: 70vh;
+    position: relative;
+
+    > .fundo {
+        width: 100%;
+        height: 100%;
+
+        > img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+    > .canto {
+        display: flex;
+        position: absolute;
+        left: 0%;
+        bottom: 0%;
+        height: 30%;
+        width: 70%;
+        
+        align-items: center;
+        background-color: v-bind(cor);
+
+        a {
+            height: 100%;
+        }
+
+        h2 {
+            text-align: center;
+        }
+
+        img {
+            height: 100%;
+            width: 100%;
+        }
+    }
+}
+</style>
---
+++ softSkills/src/views/Pagina404View.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="pagina4040">
+    <h1>erro 404</h1>
+    <h2>essa página não existe, clique no link abaixo para retornar a página home</h2>
+    <router-link :to="{ name: 'home' }">Home</router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  text-align: center;
+  font-size: 5rem;
+  color: #ff0000;
+}
+
+h2 {
+  text-align: center;
+  font-size: 2.5rem;
+}
+
+a {
+  display: block;
+  font-size: 4rem;
+  text-shadow: 0px 10px 30px rgba(0, 189, 126, 1);
+  color: rgba(0, 189, 126, 1);
+  text-align: center;
+}
+</style>
